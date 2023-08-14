2bafdabacc7357b47efd6ff31fc4a1ec70027711
sserh <serhios@bk.ru>
2023-08-14 20:39:25 +0700

    Initial commit

---
+++ src/Worker.java
@@ -0,0 +1,30 @@
+public class Worker {
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " ended with an error");
+                continue;
+            }
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+}
